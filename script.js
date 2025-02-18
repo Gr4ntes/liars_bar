342995f40934a3e9e4531077b8d20111491ec04d
Anton Grebinnyi <grants404@outlook.com>
2025-02-18 16:09:29 +0800

    added barrel to each player card and ability to play animation

--- script.js
+++ script.js
@@ -46,6 +46,75 @@ createButton.addEventListener("click", () => {
         player_card_img.src = selectedImageInput.value;
         player_card.appendChild(player_card_name);
         player_card.appendChild(player_card_img);
+        
+        // Get the container element by its ID
+        const circleCont = document.createElement('div');
+        circleCont.classList.add("circle_cont")
+        circleCont.style.animationPlayState = "paused";
+        
+        const rounds = document.createElement('div');
+        rounds.className = 'round_styles';
+        rounds.style.setProperty('--n', '6');
+        circleCont.appendChild(rounds);
+
+        let bullets = [];
+        // Function to create and append the round styles divs
+        for(let i = 1; i <= 6; i++) {
+            const round = document.createElement('div');
+            round.className = `style round ${i}`;
+            round.style.setProperty('--i', `${i+3}`);
+            
+            // Create bullet child element
+            const bullet = document.createElement('div');
+            bullet.style.animationPlayState = "paused";
+            bullet.className = `bullet _${i}`;
+            bullets.push(bullet);
+            
+            // Add inner elements to the bullet
+            const border = document.createElement('div');
+            border.className = `inner-bullet-border`;
+            
+            const inside = document.createElement('div');
+            inside.className = 'inner-bullet-inside';
+            
+            border.appendChild(inside);
+            bullet.appendChild(border);
+            
+            round.appendChild(bullet);
+            
+            // Add to circleCont
+            rounds.appendChild(round);
+        }
+        
+        circleCont.appendChild(rounds);
+        // Create and append the center div
+        const centerDiv = document.createElement('div');
+        centerDiv.className = 'center style';
+        
+        const middleDiv = document.createElement('div');
+        middleDiv.className = 'middle';
+        centerDiv.appendChild(middleDiv);
+        
+        circleCont.appendChild(centerDiv);
+        player_card.appendChild(circleCont);
+        
+
+        const shootButton = document.createElement("button");
+        shootButton.classList.add("shoot-button");
+        shootButton.innerText = "Test your luck!";
+        shootButton.addEventListener("click", async () => {
+            circleCont.style.animationPlayState = "running";
+            for (i = 0; i < bullets.length; i++ ){
+                bullets[i].style.animationPlayState = "running";
+            }
+            await new Promise(r => setTimeout(r, 700));
+            console.log("sleep finished");
+            circleCont.style.animationPlayState = "paused";
+            for (i = 0; i < bullets.length; i++ ){
+                bullets[i].style.animationPlayState = "paused";
+            }
+        });
+        player_card.appendChild(shootButton);
         container.appendChild(player_card);
     }
 });
